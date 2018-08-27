333a1d0b3263b55a99d8d23d35d02a59a86bddde
ruan65 <ruan65@gmail.com>
2018-08-27 14:52:51 +0300

    initial

---
+++ src/main/kotlin/functional/Fold.kt
@@ -0,0 +1,27 @@
+package functional
+
+val items = listOf(1, 2, 3, 4, 5)
+
+fun main(args: Array<String>) {
+    println(items)
+
+    val folded = items.fold(0) { acc, i ->
+        println("acc = $acc, i = $i")
+
+        val result = acc + i
+        result
+
+    }
+
+
+    println(folded)
+
+    val joinedToString = items.fold("") { acc, i -> if (acc.isEmpty())"$i" else "$acc $i" }
+
+    println(joinedToString)
+
+    val product = items.fold(1, Int::times)
+
+    println(product)
+
+}
